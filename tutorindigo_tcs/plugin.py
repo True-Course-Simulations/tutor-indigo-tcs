8baddaba20ba12d5ab1ce00d40ebc256e98777fc
Cannon Smith <cannon@tcsims.com>
2026-01-30 15:52:07 -0700

    fix: update npm install command for tcs-brand to use git URL

--- tutorindigo_tcs/plugin.py
+++ tutorindigo_tcs/plugin.py
@@ -120,7 +120,7 @@ for mfe in tcs_styled_mfes:
             (
                 f"mfe-dockerfile-post-npm-install-{mfe}",
                 """
-RUN npm install '@edx/brand@github:True-Course-Simulations/tcs-brand-openedx#main'
+RUN npm install '@edx/brand@git+https://github.com/True-Course-Simulations/tcs-brand-openedx.git#tcs-default'
 """,  # noqa: E501
             ),
         ]
@@ -129,7 +129,7 @@ RUN npm install '@edx/brand@github:True-Course-Simulations/tcs-brand-openedx#mai
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "mfe-dockerfile-post-npm-install-authn",
-        "RUN npm install '@edx/brand@github:True-Course-Simulations/tcs-brand-openedx#main'",
+        "RUN npm install '@edx/brand@git+https://github.com/True-Course-Simulations/tcs-brand-openedx.git#tcs-default'",
     )
 )
 
